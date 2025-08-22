5ffa16e124cd7143af7faf2dae18880cf69c3beb
Emilio Flores <emilio.flores@theksquaregroup.com>
2025-08-22 14:04:01 -0600

    Updating import

--- vite.config.js
+++ vite.config.js
@@ -1,5 +1,16 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import nodePolyfills from 'rollup-plugin-node-polyfills';
+
+export default defineConfig({
+  plugins: [nodePolyfills()],
+  resolve: {
+    alias: {
+      crypto: 'crypto-browserify',
+    },
+  },
+});
+
 
 // https://vite.dev/config/
 export default defineConfig({
