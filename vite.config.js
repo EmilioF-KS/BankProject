926a9dd77cba46f309aae8e5c0f877042e047abe
Emilio Flores <emilio.flores@theksquaregroup.com>
2025-08-23 17:49:08 -0600

    command updated

--- vite.config.js
+++ vite.config.js
@@ -4,4 +4,12 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  server: {
+    host: true, // Needed for Docker container port mapping
+    strictPort: true,
+    port: 5173, // Or your desired port
+    watch: {
+      usePolling: true, // Important for file change detection in Docker
+    },
+  },
 })
